b472e49ca4fd6828aa7de673a9c78b674708c2a1
Eric Jeffers <ejeffers.bur@gmail.com>
2020-04-19 20:39:45 -0700

    Update netlify-identity.js

--- static/admin/netlify-identity.js
+++ static/admin/netlify-identity.js
@@ -3,12 +3,12 @@
  * https://www.netlifycms.org/docs/add-to-your-site/#add-the-netlify-identity-widget
  */
 
-if (window.netlifyIdentity) {
-    window.netlifyIdentity.on('init', function(user) {
-        if (!user) {
-            window.netlifyIdentity.on('login', function() {
-                document.location.href = '/admin/';
-            });
-        }
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
     });
-}
+  }
